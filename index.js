811a53c1fd530fc85caae6727662fae4f5cf2bd8
Thyfanne Dayane Cosme do Nascimento <thyfanne.d@academico.ifrn.edu.br>
2022-12-14 17:01:31 -0300

    pag1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const server = express ();
+const paginas = require ('./src/data/paginas.json');
+
+server.get('/paginas', (req,res) => {
+    return res.json(paginas)
+})
+server.listen(3000, () => {
+
+    console.log('servidor on')
+});
