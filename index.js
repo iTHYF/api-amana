9bfd8b648e5e26eaae3df4fd1ea3f3e7b5ab3c1a
Thyfanne Dayane Cosme do Nascimento <thyfanne.d@academico.ifrn.edu.br>
2022-12-15 16:50:59 -0300

    error-fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express ();
 const paginas = require ('./src/data/paginas.json');
 
-server.get('/', (req,res) => {
+server.get('/src/data/paginas.json', (req,res) => {
     return res.json(paginas)
 })
 server.listen(3000, () => {
